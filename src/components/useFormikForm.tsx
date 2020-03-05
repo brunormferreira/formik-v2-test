cdc58d2c6b4c185666bd9d9994c0b3cc00d0b9c7
brunormferreira <brunormferreira@hotmail.com>
2020-03-04 22:17:57 -0300

    add understanding getFieldProps

--- src/components/useFormikForm.tsx
+++ src/components/useFormikForm.tsx
@@ -4,12 +4,12 @@ import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
   firstName: Yup.string().max(16, 'Too big').min(2, 'To small').required('First Name Required'),
-  lastName: Yup.string().required('Last Name Required'),
+  lastName: Yup.string().max(20, 'Too big').required('Last Name Required'),
   email: Yup.string().email().required('E-mail Required')
 })
 
 const MyForm: React.FC = () => {
-  const { handleSubmit, handleChange, values, errors, handleBlur } = useFormik({
+  const { handleSubmit, errors, getFieldProps } = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
@@ -26,32 +26,20 @@ const MyForm: React.FC = () => {
     <form onSubmit={handleSubmit} style={{display: 'inline-grid'}}>
       <label htmlFor="firstName">First Name</label>
       <input
-        id="firstName"
         name="firstName"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.firstName}
+        {...getFieldProps('firstName')}
       />
       {errors.firstName && errors.firstName}
       <label htmlFor="lastName">Last Name</label>
       <input
-        id="lastName"
         name="lastName"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.lastName}
+        {...getFieldProps('lastName')}
       />
       {errors.lastName && errors.lastName}
       <label htmlFor="email">Email Address</label>
       <input
-        id="email"
         name="email"
-        type="email"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.email}
+        {...getFieldProps('email')}
       />
       {errors.email && errors.email}
       <button type="submit">Submit</button>
